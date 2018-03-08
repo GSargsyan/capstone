32d6cb193f2ef232515e36fa8486883d3c3ada40
gsargsyan <g.sargsyan1995@gmail.com>
2018-03-08 09:58:27 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+dnf install gcc-c++
+pip3 install -r requirements.txt
+
+mkdir -p var/images
---
+++ src/module_imports.py
@@ -0,0 +1,9 @@
+# import the necessary packages
+from imutils import face_utils
+import numpy as np
+import argparse
+import imutils
+import dlib
+import cv2
+import pprint
+import random
